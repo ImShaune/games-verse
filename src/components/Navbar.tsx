1a413998d78765c5d4ee95478e4f7e0884c57bb6
ImShaune <nndesigner@outlook.es>
2024-02-26 22:55:25 -0300

    Navbar progess

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,28 +1,46 @@
-"use client"
-import Link from 'next/link';
-import { useRouter } from 'next/navigation';
-import React, { useState } from 'react'
+"use client";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
 const Navbar = () => {
+  const [input, setInput] = useState("");
+  const router = useRouter();
 
-    const [input, setInput] = useState("")
-    const router = useRouter()
+  const searchGame = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    router.push(`?movie=${input}`);
+    setInput("");
+  };
 
-    const searchGame = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
-        router.push(`?game=${input}`)
-        setInput("")
-    };
-  
-    return <div className="bg-primary pt-4 px-4 md:px-0">
-        <div className='container mx-auto flex justify-between items-center'>
+  return (
+    <div className="bg-primary py-4 px-4 md:px-0">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href="/">
+          <div className="text-[30px] font-medium">Logo</div>
+        </Link>
 
-<Link href="/">
-    <div className='text-[30px] font-medium'>Logo</div>
-</Link>
-            
-        </div>
+        <form onSubmit={searchGame}>
+          <div className="space-x-4">
+            <input
+              className="bg-secondary px-4 py-2 outline-none placeholder:text-textColor"
+              type="text"
+              value={input}
+              placeholder="Search a game..."
+              onChange={(e) => setInput(e.target.value)}
+            />
+
+            <button
+              type="submit"
+              className="bg-secondary text-textColor py-2 px-4 hover:bg-textColor hover:text-white"
+            >
+              Search
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
